8b5c947f40db9e870ba9ca6a754726930072a818
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-25 22:54:39 +0530

    Program to guess the coupon Numbers Entered by User

--- src/logicalProg/couponNumber.java
+++ src/logicalProg/couponNumber.java
@@ -1,5 +1,6 @@
 package logicalProg;
 import java.util.Scanner;
+import java.util.ArrayList;
 public class couponNumber {
 /**
  * 
@@ -14,17 +15,19 @@ public class couponNumber {
 		 * Added a WHILE Loop to check the Input Numbers by Random Numbers
 		 * By Using Count, We can check the number of Iterations
 		 * Taken If Condition to check if (Number = Random Number) or not
-		 * if YES, It will break and Print the Guessed Number
+		 * Taken an ArrayList to store the Random Numbers to Print at last.
+		 * if condition is YES, It will break and store to Array.
 		 * At last, We will get the count value and print How many Random Numbers taken to guess Input Nmber
 		 */
 		System.out.println("WELCOME TO COUPON NUMBER CHECKER");
 		System.out.println("");
 		int number,numbers=0,count=1;
-		System.out.println("Enter How Many Numbers You Want");
+		System.out.println("Enter How Many Numbers You Want to Input");
 		Scanner sc = new Scanner(System.in);
 		number = sc.nextInt();
+		ArrayList<Integer> arrayList = new ArrayList<Integer>(number);
+		System.out.println("Enter 3 Numbers (Between 100-999) and Press ENTER");
 		for(int i =1 ; i<=number;i++) {
-			System.out.println("Enter the Number");
 			Scanner sc2 = new Scanner(System.in);
 			numbers = sc2.nextInt();
 			while(number!=0){
@@ -34,12 +37,19 @@ public class couponNumber {
 					int random =(int) Math.floor(Math.random()*(999)+100);
 					//System.out.println("The Number is "+random);
 					if(random == numbers) {
-						System.out.println("Number you Entered is "+random);
+						arrayList.add(random);
 						break;
 					}
 			}
 	
-		}		
+		}	
+		System.out.println("Numbers you Entered : ");
+		System.out.println("");
+		for (int i=0;i<number;i++) {
+			int current = arrayList.get(i);
+			System.out.println(current);
+			System.out.println("");
+		}
 		System.out.println("Random Numbers Generated to find Input Numbers is "+count);
 	}
 
